614dee6776845b13c2921b06fac48011b3f0785a
Valentin Anklin <valentin@ankl.in>
2020-05-04 16:44:13 +0200

    Add basic task

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -1,31 +1,15 @@
 # -*- coding: utf-8 -*-
-import click
+import os
 import logging
-from pathlib import Path
-from dotenv import find_dotenv, load_dotenv
-import sklearn
+import numpy as np
+from sklearn import datasets
+from sklearn.model_selection import train_test_split
 
+data_path = os.path.join("..", "data")
+iris = datasets.load_iris()
 
-@click.command()
-@click.argument('input_filepath', type=click.Path(exists=True))
-@click.argument('output_filepath', type=click.Path())
-def main(input_filepath, output_filepath):
-    """ Runs data processing scripts to turn raw data from (../raw) into
-        cleaned data ready to be analyzed (saved in ../processed).
-    """
-    logger = logging.getLogger(__name__)
-    logger.info('making final data set from raw data')
-
-
-if __name__ == '__main__':
-    log_fmt = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
-    logging.basicConfig(level=logging.INFO, format=log_fmt)
-
-    # not used in this stub but often useful for finding various files
-    project_dir = Path(__file__).resolve().parents[2]
-
-    # find .env automagically by walking up directories until it's found, then
-    # load up the .env entries as environment variables
-    load_dotenv(find_dotenv())
-
-    main()
+X_train, X_test, y_train, y_test = train_test_split(iris.data, iris.target, test_size=0.3)
+np.save(os.path.join(data_path, "X_train.npy"), X_train)
+np.save(os.path.join(data_path, "X_test.npy"), X_test)
+np.save(os.path.join(data_path, "y_train.npy"), y_train)
+np.save(os.path.join(data_path, "y_test.npy"), y_test)
--- src/models/train_model.py
+++ src/models/train_model.py
@@ -1,3 +1,29 @@
-import sklearn
+import os
+import logging
+from joblib import dump
+import numpy as np
+from sklearn.svm import SVC
+from sklearn.model_selection import GridSearchCV
+from sklearn.preprocessing import StandardScaler
 
-iris = sklearn.datasets.load_iris()
+data_path = os.path.join("..", "data")
+model_path = os.path.join("..", "models")
+
+X_raw = np.load(os.path.join(data_path, 'X_train.npy'))
+y = np.load(os.path.join(data_path, 'y_train.npy'))
+
+scaler = StandardScaler()
+X = scaler.fit_transform(X_raw)
+
+scaler_path = os.path.join(model_path, "scaler.joblib")
+dump(scaler, scaler_path)
+
+kernel = "linear"
+gamma = 0.01
+C = 0.002
+
+model = SVC(kernel=kernel, gamma=gamma, C=C)
+model.fit(X=X, y=y)
+
+model_path = os.path.join(model_path, "model.joblib")
+dump(model, model_path)
